ff4f6aa763e15552daab1f577b2789d1047438ed
BgMg <brandong.manzog@gmail.com>
2022-09-27 20:25:19 -0600

    end practice

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use regex::Regex;
-
 fn main() {
+    let mut nombres: Vec<String> = Vec::new();
+
+    for _i in 0..3 {
+        println!("Por favor introduce un nombre: ");
+        let mut nombre = String::new();
+        std::io::stdin().read_line(&mut nombre).unwrap();
+
+        nombres.push(nombre);
+    }
+
+    for nombre in nombres {
+        println!("El nombred es: {}", nombre);
+    }
     
+    let hola = ["H", "O", "L", "A"];
+    for letter in hola {
+        println!("Letra: {}", letter);
+    }
 }
